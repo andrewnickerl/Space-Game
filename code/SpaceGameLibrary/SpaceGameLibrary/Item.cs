147a91e9e50439300545ef50eb9e483201612fe6
CharlesG4 <59873920+CharlesG4@users.noreply.github.com>
2020-02-13 10:28:46 -0500

    Added 2 Classes Updated Diagram
    Added - Class/ Item.cs
    Added - Class/SpaceEvents.cs
    Updated -SpaceGameClassDiagram.cd

---
+++ code/SpaceGameLibrary/SpaceGameLibrary/Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceGameLibrary
+{
+    public class Item
+    {
+        public string name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string description
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int damage
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Value
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public bool reusable
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ code/SpaceGameLibrary/SpaceGameLibrary/SpaceEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceGameLibrary
+{
+    public class SpaceEvent
+    {
+        public int identifier
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public bool Story(Array story)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
