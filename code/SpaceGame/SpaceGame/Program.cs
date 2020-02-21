fdcc158edad02fd8486a9f62eafc2dc8d26086f9
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-21 08:59:39 -0500

    Started to test instantiating objects/testing methods

--- code/SpaceGame/SpaceGame/Program.cs
+++ code/SpaceGame/SpaceGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SpaceGameLibrary;
+using System.Collections.Generic;
 
 namespace SpaceGame
 {
@@ -7,7 +8,18 @@ namespace SpaceGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Player pc = new Player();
+            //BattleEntity shark = new BattleEntity();
+            //string selection = Console.ReadLine();
+            //Planet noir = new Planet(Enumerations.PlanetTypes.Noir);
+
+            //noir.Combat(pc, shark);
+
+            //foreach (Item item in pc.Inventory)
+            //{
+            //    Console.WriteLine(item.Name);
+            //}
+            //Console.ReadKey();
         }
     }
 }
