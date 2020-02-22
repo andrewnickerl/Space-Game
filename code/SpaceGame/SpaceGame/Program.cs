72dca88ec14eb131e030f37943a0e540ed6600e1
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-21 19:25:13 -0500

    Revert "Create for loop to manipulate story within planets"
    This reverts commit 95a15bd7cfec3e3cbf5fc03a506226ddeb65ec63.

--- code/SpaceGame/SpaceGame/Program.cs
+++ code/SpaceGame/SpaceGame/Program.cs
@@ -8,86 +8,85 @@ namespace SpaceGame
     {
         static void Main(string[] args)
         {
-            //Player pc = new Player();
-            //pc.Name = "PlayerName";
-            //pc.PetName = "PetName";
-            //Planet noir = new Planet(Enumerations.PlanetTypes.Noir, pc);
-            //Planet aventus = new Planet(Enumerations.PlanetTypes.Aventus, pc);
-            //Planet spotMee = new Planet(Enumerations.PlanetTypes.SpotMee, pc);
-            //Planet wombodum = new Planet(Enumerations.PlanetTypes.Wombodum, pc);
-            //Planet krytunga = new Planet(Enumerations.PlanetTypes.Krytunga, pc);
+            Player pc = new Player();
+            pc.Name = "PlayerName";
+            pc.PetName = "PetName";
+            Planet noir = new Planet(Enumerations.PlanetTypes.Noir, pc);
+            Planet aventus = new Planet(Enumerations.PlanetTypes.Aventus, pc);
+            Planet spotMee = new Planet(Enumerations.PlanetTypes.SpotMee, pc);
+            Planet wombodum = new Planet(Enumerations.PlanetTypes.Wombodum, pc);
+            Planet krytunga = new Planet(Enumerations.PlanetTypes.Krytunga, pc);
 
-            //foreach (var output in noir.Story)
-            //{
-            //    Console.WriteLine(output);
-            //}
-            run();
+            foreach (var output in noir.Story)
+            {
+                Console.WriteLine(output);
+                Console.Clear();
+            }
+
             Console.ReadKey();
         }
         static void run()
         {
             Player pc = new Player();
-            pc.Name = "Andrew";
-            pc.PetName = "Jackson";
+            //pc.Name = "Andrew";
+            //pc.PetName = "Jackson";
 
-            //Console.Write("What is your name?: ");
-            //pc.Name = Console.ReadLine();
-            //Console.Write("What is the name of your beloved fur baby? ");
-            //pc.PetName = Console.ReadLine();
+            Console.Write("What is your name?: ");
+            pc.Name = Console.ReadLine();
+            Console.Write("What is the name of your beloved fur baby? ");
+            pc.PetName = Console.ReadLine();
 
             Planet noir = new Planet(Enumerations.PlanetTypes.Noir, pc);
 
-            for(int i = 0; i < noir.Story.Count; i++)
+            foreach (var item in noir.Story)
             {
+                Console.WriteLine(item);
                 Console.Clear();
-                Console.WriteLine(noir.Story[i]);
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadKey();
             }
 
-            //Planet aventus = new Planet(Enumerations.PlanetTypes.Aventus, pc);
+            Planet aventus = new Planet(Enumerations.PlanetTypes.Aventus, pc);
 
-            //foreach (var item in aventus.Story)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (var item in aventus.StoreInventory)
-            //{
-            //    Console.WriteLine(item.Name);
-            //}
+            foreach (var item in aventus.Story)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in aventus.StoreInventory)
+            {
+                Console.WriteLine(item.Name);
+            }
 
-            //Planet spotMee = new Planet(Enumerations.PlanetTypes.SpotMee, pc);
+            Planet spotMee = new Planet(Enumerations.PlanetTypes.SpotMee, pc);
 
-            //foreach (var item in spotMee.Story)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (var item in spotMee.StoreInventory)
-            //{
-            //    Console.WriteLine(item.Name);
-            //}
+            foreach (var item in spotMee.Story)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in spotMee.StoreInventory)
+            {
+                Console.WriteLine(item.Name);
+            }
 
-            //Planet wombodum = new Planet(Enumerations.PlanetTypes.Wombodum, pc);
+            Planet wombodum = new Planet(Enumerations.PlanetTypes.Wombodum, pc);
 
-            //foreach (var item in wombodum.Story)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (var item in wombodum.StoreInventory)
-            //{
-            //    Console.WriteLine(item.Name);
-            //}
+            foreach (var item in wombodum.Story)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in wombodum.StoreInventory)
+            {
+                Console.WriteLine(item.Name);
+            }
 
-            //Planet krytunga = new Planet(Enumerations.PlanetTypes.Krytunga, pc);
+            Planet krytunga = new Planet(Enumerations.PlanetTypes.Krytunga, pc);
 
-            //foreach (var item in krytunga.Story)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //foreach (var item in krytunga.StoreInventory)
-            //{
-            //    Console.WriteLine(item.Name);
-            //}
+            foreach (var item in krytunga.Story)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in krytunga.StoreInventory)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
     }
 }
