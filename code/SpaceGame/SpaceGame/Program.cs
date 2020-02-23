5fb76e07d184333b33c7c568cbb0e6bca5995f57
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-22 22:34:51 -0500

    Add title page, font customization, associated system libraries

--- code/SpaceGame/SpaceGame/Program.cs
+++ code/SpaceGame/SpaceGame/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using SpaceGameLibrary;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
 using System.Threading;
 
 namespace SpaceGame
@@ -9,7 +13,74 @@ namespace SpaceGame
     {
         static void Main(string[] args)
         {
+            var arr = new[]
+{
+
+
+                      @"   ***** *    **   ***                                     *                            * ***",
+                    @"  ******  *  *****    ***                                  **                           *  ****                                           *",
+                   @"  **   *  *     *****   ***                                 **                          *  *  ***                                         **",
+                 @"   *    *  **     * **      **                                **                         *  **   ***                                        **",
+                 @"       *  ***     *         ** **   ****                      **            ****        *  ***    *** **   ****                  ****     ********",
+                 @"      **   **     *         **  **    ***  * *** **** ****    ** ****      * ***  *    **   **     **  **    ***  *    ***      * **** * ********",
+                 @"      **   **     *         **  **     ****   *** **** ***  * *** ***  *  *   ****     **   **     **  **     ****    * ***    **  ****     **",
+                   @"    **   **     *         **  **      **     **  **** ****  **   ****  **    **      **   **     **  **      **    *   ***  ****          **",
+                   @"    **   **     *         **  **      **     **   **   **   **    **   **    **      **   **     **  **      **   **    ***   ***         **     ",
+                    @"   **   **     *         **  **      **     **   **   **   **    **   **    **      **   **     **  **      **   ********      ***       **     ",
+                     @"   **  **     *         **  **      **     **   **   **   **    **   **    **       **  ** *** **  **      **   *******         ***     **     ",
+                      @"   ** *      *         *   **      **     **   **   **   **    **   **    **        ** *   ****   **      **   **         ****  **     **     ",
+                      @"    ***      ***      *     ******* **    **   **   **   **    **    ******          ***     ***   ******* **  ****    * * **** *      **     ",
+                      @"     ******** ********       *****   **   ***  ***  ***   *****       ****            ******* **    *****   **  *******     ****        **    ",
+                       @"      ****     ****                       ***  ***  ***   ***                          ***   **                 *****                        ",
+                          @"                                                                                         **                                              ",
+                             @"                                                                                       *                                               ",
+                                @"                                                                                   *                                                ",
+                                   @"                                                                               *                                                 ",
+
+
+            };
+            Console.WindowWidth = 170;
+            Console.WriteLine("\n\n");
+            var maxLength = arr.Aggregate(0, (max, line) => Math.Max(max, line.Length));
+            var x = Console.BufferWidth / 2 - maxLength / 2;
+            for (int y = -arr.Length; y < Console.WindowHeight + arr.Length; y++)
+            {
+                ConsoleDraw(arr, x, y);
+                Thread.Sleep(100);
+            }
+            Console.ReadKey();
             run();
+        }
+
+        static void ConsoleDraw(IEnumerable<string> lines, int x, int y)
+        {
+            if (x > Console.WindowWidth) return;
+            if (y > Console.WindowHeight) return;
+
+            var trimLeft = x < 0 ? -x : 0;
+            int index = y;
+
+            x = x < 0 ? 0 : x;
+            y = y < 0 ? 0 : y;
+
+            var linesToPrint =
+                from line in lines
+                let currentIndex = index++
+                where currentIndex > 0 && currentIndex < Console.WindowHeight
+                select new
+                {
+                    Text = new String(line.Skip(trimLeft).Take(Math.Min(Console.WindowWidth - x, line.Length - trimLeft)).ToArray()),
+                    X = x,
+                    Y = y++
+                };
+
+            Console.Clear();
+            foreach (var line in linesToPrint)
+            {
+                Console.SetCursorPosition(line.X, line.Y);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.Write(line.Text);
+            }
         }
         static void run()
         {
